d9fc938f74013b01d3cc4e598da0e457a1b383e1
carlos123asd <carlos-medin@hotmail.com>
2025-06-24 16:32:54 +0200

    ADD: swagger routes Cliente

--- src/http/routes/Cliente.routes.ts
+++ src/http/routes/Cliente.routes.ts
@@ -11,7 +11,7 @@ const routerCliente = Router()
 
 /**
  * @swagger
- * /clientes/crear:
+ * /cliente/crear:
  *   post:
  *     summary: Crear un nuevo cliente
  *     tags: [Clientes]
@@ -26,14 +26,22 @@ const routerCliente = Router()
  *                 type: string
  *               email:
  *                 type: string
+ *               direccion:
+ *                  type: string
+ *               password:
+ *                  type: string
+ *               metodoPago:
+ *                  type: string
  *     responses:
  *       201:
  *         description: Cliente creado correctamente
+ *       404:
+ *         description: Error al crear Cliente
  */
 routerCliente.post('/crear',ClienteController.crearCliente)
 /**
  * @swagger
- * /clientes/modificar:
+ * /cliente/modificar:
  *   put:
  *     summary: Modificar un cliente
  *     tags: [Clientes]
@@ -43,12 +51,96 @@ routerCliente.post('/crear',ClienteController.crearCliente)
  *         application/json:
  *           schema:
  *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *               nombre:
+ *                 type: string
+ *               email:
+ *                  type: string
+ *               direccion:
+ *                  type: string
+ *               password:
+ *                  type: string
+ *               metodoPago:
+ *                  type: string
  *     responses:
  *       200:
  *         description: Cliente modificado correctamente
+ *       404:
+ *         description: Error al modificar un cliente
  */
 routerCliente.put('/modificar',ClienteController.modificarCliente)
+/**
+ * @swagger
+ * /cliente/eliminar:
+ *   delete:
+ *     summary: Eliminar un cliente
+ *     tags: [Clientes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *               nombre:
+ *                 type: string
+ *               email:
+ *                  type: string
+ *               direccion:
+ *                  type: string
+ *               password:
+ *                  type: string
+ *               metodoPago:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: Cliente eliminado correctamente
+ *       404:
+ *         description: Error al eliminar un cliente
+ */
 routerCliente.delete('/eliminar',ClienteController.eliminarCliente)
+/**
+ * @swagger
+ * /cliente/buscar/id/{id}:
+ *   get:
+ *     summary: Buscar un cliente por ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID del cliente
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Cliente encontrado correctamente por ID
+ *       404:
+ *         description: Error al buscar un cliente por ID
+ */
 routerCliente.get('/buscar/id/:id',ClienteController.buscarPorId)
+/**
+ * @swagger
+ * /cliente/buscar/email/{email}:
+ *   get:
+ *     summary: Buscar un cliente por email
+ *     tags: [Clientes]
+ *     parameters:
+ *       - name: email
+ *         in: path
+ *         required: true
+ *         description: email unico del cliente
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Cliente encontrado correctamente por email
+ *       404:
+ *         description: Error al buscar un cliente por email
+ */
 routerCliente.get('/buscar/email/:email',ClienteController.buscarPorEmail)
 export default routerCliente
